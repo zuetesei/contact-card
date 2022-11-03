ad0b162a92296f1dc9bdba893043fd311d9637d5
zue <zuleikatesei@gmail.com>
2022-11-03 14:36:42 -0700

    submit button not working

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -19,6 +19,34 @@ window.addEventListener('load', function () {
     document.getElementById('dogThumbnail').src = Dog;
 });
 
+window.editCard = (e) => {
+    //grabs id from button element attached to contact card and sets global var that will be used in the form element
+    profileId = parseInt(e.dataset.id);
+
+    // grabs info to pre-populate edit form
+    let editName = e.dataset.name;
+    let editEmail = e.dataset.email;
+    let editPhone = e.dataset.phone;
+
+    document.getElementById("name").value = editName;
+    document.getElementById("email").value = editEmail;
+    document.getElementById("phone").value = editPhone;
+
+    form.style.display = "block";
+
+    // toggles the submit button so that it now updates an existing contact instead of posting a new one
+    submitBtnToUpdate = true;
+}
+
+window.deleteCard = (e) => {
+    let id = parseInt(e.id);
+    // delete the card
+    deleteDb(id);
+
+    // reload the DOM
+    fetchCards();
+};
+
 // Form functionality
 const form = document.getElementById("formToggle");
 const newContactButton = document.getElementById("new-contact");
@@ -61,33 +89,6 @@ form.addEventListener('submit', event => {
     fetchCards();
 });
 
-window.editCard = (e) => {
-    //grabs id from button element attached to contact card and sets global var that will be used in the form element
-    profileId = parseInt(e.dataset.id);
-
-    // grabs info to pre-populate edit form
-    let editName = e.dataset.name;
-    let editEmail = e.dataset.email;
-    let editPhone = e.dataset.phone;
-
-    document.getElementById("name").value = editName;
-    document.getElementById("email").value = editEmail;
-    document.getElementById("phone").value = editPhone;
-
-    form.style.display = "block";
-
-    // toggles the submit button so that it now updates an existing contact instead of posting a new one
-    submitBtnToUpdate = true;
-}
-
-window.deleteCard = (e) => {
-    let id = parseInt(e.id);
-    // delete the card
-    deleteDb(id);
-
-    // reload the DOM
-    fetchCards();
-};
 
 
 
