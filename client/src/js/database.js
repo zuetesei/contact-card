187435794d3ff944822c78fd792fd3325a50d9cb
zue <zuleikatesei@gmail.com>
2022-11-03 12:36:35 -0700

    created and test getDb and Postdb functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,3 +16,45 @@ export const initdb = async () => {
         }
     })
 }
+
+// export function getDb to GET to the db
+export const getDb = async () => {
+    console.log('Get from the database...');
+
+    // create a connection to indexedDB db and the version we want to use
+    const contactDb = await openDB('contact_db', 1);
+
+    // create a new transaction and specify the store and data priveleges
+    const tx = contactDb.transaction('contacts', 'readonly');
+
+    // open up the desired object store
+    const store = tx.objectStore('contacts');
+
+    // use .getAll() method to get data in db
+    const request = store.getAll();
+
+    // get confirmation of the request
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+};
+
+export const postDb = async (name, email, phone, profile) => {
+    console.log('POST to the database...');
+
+    // create connection to db and specify the version 
+    const contactDb = await openDB('contact_db', 1);
+
+    // create a new transaction and specify the store and data priveleges
+    const tx = contactDb.transaction('contacts', 'readwrite');
+
+    // open up the desired object store
+    const store = tx.objectStore('contacts');
+
+    // use the .add() method on the store and pass in the content
+    const request = store.add({ name: name, email: email, phone: phone, profile: profile });
+
+    // get confirmation of the request
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+};
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,7 +3,7 @@ import "./submit";
 import "../css/index.css";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initdb } from "./database";
+import { initdb, getDb, postDb } from "./database";
 
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
@@ -11,6 +11,10 @@ import Dog from "../images/dog.png";
 
 window.addEventListener('load', function () {
     initdb();
+    // temporarily placing getDb and postDb here for testing
+    getDb();
+    postDb("Zue", "zuleika@gmail.com", 920808309, "Bear");
+    getDb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
