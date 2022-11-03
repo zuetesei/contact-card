ad8d8dcca10fdbd5f70fb41357d5e4c244813fc8
zue <zuleikatesei@gmail.com>
2022-11-03 12:37:07 -0700

    deleted getDb and postDb after testing

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -11,10 +11,6 @@ import Dog from "../images/dog.png";
 
 window.addEventListener('load', function () {
     initdb();
-    // temporarily placing getDb and postDb here for testing
-    getDb();
-    postDb("Zue", "zuleika@gmail.com", 920808309, "Bear");
-    getDb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
